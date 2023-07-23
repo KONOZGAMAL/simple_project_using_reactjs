160765f49f7f273b189528b04330cb228b3c5724
konoozgamal <aya476771@gmail.com>
2023-07-23 12:17:26 +0300

    first commit

---
+++ src/Components/Data.jsx
@@ -0,0 +1,45 @@
+const data = [
+  {
+    id: 1,
+    url: "./Img/gallery.jpeg",
+    link: "parentgallery",
+    nameproject: "Responsive Media Gallery",
+    alt:'logo1',
+  },
+  {
+    id: 2,
+    url: "./Img/list.jpeg",
+    link: "/parenttodo",
+    nameproject: "To Do List",
+    alt:'logo2',
+  },
+  {
+    id: 3,
+    url: "./Img/randomnumber.jpeg",
+    link: "/randomnumber",
+    nameproject: "Random Number",
+    alt:'logo3',
+  },
+  {
+    id: 4,
+    url: "./Img/count.jpeg",
+    link: "/count",
+    nameproject: "Count",
+    alt:'logo4',
+  },
+  {
+    id: 5,
+    url: "./Img/phone.jpeg",
+    link: "/phone",
+    nameproject: "Phone",
+    alt:'logo5',
+  },
+  {
+    id: 6,
+    url: "./Img/phone.jpeg",
+    link: "/navbar",
+    nameproject: "AnimatedContdown",
+    alt:'logo6',
+  },
+];
+export default data;
---
+++ src/Components/ParentAllProjects.jsx
@@ -0,0 +1,26 @@
+import {React } from 'react';
+// import {useNavigate} from "react-router-dom";
+import Data from './Data';
+import Style from './ComponentAll.module.css';
+export default function ParentAllProjects() {
+    // const navigate = useNavigate() onClick={()=>{navigate(e.link)}}
+    console.log(window.width);
+  return (
+    <div className={Style.parent}>
+        <div className={Style.title}>
+        <h1>Simple React Js Projects</h1>
+        <h3> All Projects</h3>
+        </div>
+        <div className={Style.items}>
+        {
+            Data.map((e)=>(
+                <div key={e.id} className={Style.item}>
+                    <img src={e.url} className={Style.imgs} alt={e.alt}/>
+                    <button to={e.link} className={Style.btn}>{e.nameproject}</button>
+                </div>
+            ))
+        }
+        </div>
+    </div>
+  )
+}
